724879d0d3b908603cd8f9afa1da3addce06e320
Pradeepakumara K R <pradeepkumarkaggundi@gmail.com>
2023-02-14 22:16:41 -1200

    Updated

---
+++ src/api/api.js
@@ -0,0 +1,3 @@
+export const BASE_URL = "https://birdscollectionbackend.onrender.com";
+
+// export const BASE_URL = "http://localhost:8003";
---
+++ src/components/Main.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import { BASE_URL } from '../api/api';
+import MainPosts from './MainPosts';
+
+const Main = () => {
+
+    const [postData, setPostData] = useState([]);
+    console.log(postData);
+
+    const getData = async () => {
+
+        const res = await fetch(`${BASE_URL}/getdata`, {
+            method: "GET",
+            headers:{
+                "Content-Type":"application/json"
+            }
+        });
+
+        const data = await res.json();
+        console.log(data);
+
+        if(res.status === 422 || !data){
+            console.log("error")
+        }
+        else{
+            setPostData(data)
+            console.log("get data")
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, [])
+
+
+  return (
+    <div className='container mt-3'>
+        <div className="row">
+        {
+            postData.map(post => <MainPosts key={post._id} post={post} />)
+        }
+        </div>
+    </div>
+  )
+}
+
+export default Main
